8973def485ea81a1857ae5ac0db33681367eb98f
geheimerr <tejas.shandilya24@gmail.com>
2025-07-29 22:14:48 +0530

    Rectify code to ensure booking states are kept intact

--- main.go
+++ main.go
@@ -3,29 +3,48 @@ package main
 import "fmt"
 
 func main() {
-	var confName string = "GoCon"
+	confName := "GoCon"
 	const conftix int = 50
 	var remtix uint = 50
+	var bookings []string
 
 	fmt.Println("Welcome to TixCLI.")
-	fmt.Println("Get your tickets to attend", confName)
-	fmt.Println("Total GoCon tickets:", conftix, "		Remaining GoCon tickets:", remtix)
+	fmt.Printf("Get your tickets to attend %v\n", confName)
+	fmt.Printf("Total GoCon tickets: %v    Remaining GoCon tickets: %v\n", conftix, remtix)
+	fmt.Println("--------------------------------------------------")
 
-	var userName string
-	var usertix uint
-	var userEmail string
-	fmt.Printf("Please enter your username: %v", userName)
-	fmt.Scan(&userName)
-	fmt.Printf("Hi %v, how many tickets do you want? ", userName)
-	fmt.Scan(&usertix)
-	fmt.Print("Enter your email ID. The purchased tickets shall be sent the email ID entered: ")
-	fmt.Scan(&userEmail)
-	fmt.Printf("%v tickets purchased succesfully. Your tickets have been sent to %v \n", usertix, userEmail)
-	var updatedtixCount = remtix - usertix
-	var bookings []string
-	bookings = append(bookings, userName)
+	for {
+		if remtix == 0 {
+			fmt.Println("Sorry, all tickets for GoCon have been sold out. Thank you!")
+			break
+		}
 
-	fmt.Printf("Current bookings: %v\n", bookings)
-	fmt.Printf("%v tickets remaining\n", updatedtixCount)
+		var userName string
+		var usertix uint
+		var userEmail string
 
+		fmt.Print("Please enter your username: ")
+		fmt.Scan(&userName)
+
+		fmt.Printf("Hi %v, how many tickets do you want? ", userName)
+		fmt.Scan(&usertix)
+
+		if usertix > remtix {
+			fmt.Printf("We're sorry, you can't purchase %v tickets. We only have %v remaining.\n", usertix, remtix)
+			fmt.Println("--------------------------------------------------")
+			continue
+		}
+
+		fmt.Print("Enter your email ID. The purchased tickets will be sent to this address: ")
+		fmt.Scan(&userEmail)
+
+		remtix -= usertix
+		bookings = append(bookings, userName)
+
+		fmt.Printf("\nThank you, %v! %v tickets purchased successfully.\n", userName, usertix)
+		fmt.Printf("A confirmation has been sent to %v\n", userEmail)
+		fmt.Printf("Current bookings: %v\n", bookings)
+		fmt.Printf("%v tickets remaining for %v.\n", remtix, confName)
+		fmt.Println("--------------------------------------------------")
+	}
 }
